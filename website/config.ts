50d45c1f05ddb0414eacf7a2c0d4c383ed13ff87
Schlauer-Hax <arzberger52@gmail.com>
2021-10-05 16:28:16 +0200

    Added Website Config

---
+++ website/config.ts
@@ -0,0 +1,15 @@
+export const config = {
+    title: "Hax - COO of BBN",
+    links: [
+        { title: "Discord", url: "https://discord.gg/nPwjaJk" },
+        { title: "GitHub", url: "https://github.com/Schlauer-Hax" },
+        { title: "Instagram", url: "https://www.instagram.com/haxhd/" },
+        { title: "Twitter", url: "https://twitter.com/Hax6775" },
+    ],
+    navigationElements: [
+        { title: 'HaxIs.me', url: 'https://haxis.me' },
+        { title: 'GitHub', url: 'https://github.com/Schlauer-Hax' },
+        { title: 'BBN', url: 'https://bbn.one' },
+        { title: 'Contact', url: 'mailto:mail@haxis.me' }
+    ]
+};
