dfe9c1707257bd70e4a2817662227e9f925afd4d
Schlauer-Hax <hax@bbn.family>
2023-10-14 22:27:33 +0200

    center buttons

--- website/src/components/socials.ts
+++ website/src/components/socials.ts
@@ -1,15 +1,16 @@
 
-import { Button, ButtonStyle, Horizontal, Spacer } from "webgen/mod.ts";
+import { Button, ButtonStyle, Grid, Horizontal, Spacer } from "webgen/mod.ts";
 import { config } from '../../config.ts';
 
 export function renderSocials() {
     return Horizontal(
         Spacer(),
-        config.links.map(link => {
+        Grid(...config.links.map(link => {
             return Button(link.title)
                 .asLinkButton(link.url)
                 .setStyle(ButtonStyle.Secondary)
-        }),
+                .setJustify('center')
+        })).setEvenColumns(config.links.length).setGap('var(--gap)'),
         Spacer(),
-    ).setMargin('0.5rem').setGap('0.75rem');
+    );
 }
